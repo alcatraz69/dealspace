1e549b24a67d79dacfd11950a74ca527402aea06
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2020-05-10 16:55:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./compnents/Header";
+import Searchbar from "./compnents/Searchbar";
+import Section1 from "./compnents/Section1";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Searchbar />
+      <Section1 />
     </div>
   );
 }
---
+++ src/compnents/Searchbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Searchbar() {
+  return (
+    <div>
+      <input type="text" placeholder="Your Deal Space" />
+    </div>
+  );
+}
+
+export default Searchbar;
