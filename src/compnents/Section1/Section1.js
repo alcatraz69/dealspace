d2108f47a122e7af43a50edc4e6b14d37f7f150d
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2020-05-20 00:04:47 +0530

    tablet responsive

--- src/compnents/Section1/Section1.js
+++ src/compnents/Section1/Section1.js
@@ -75,21 +75,23 @@ function Section1(props) {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+          // nextArrow: <SampleNextArrow />,
+          // prevArrow: <SamplePrevArrow />,
         },
       },
       {
         breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
